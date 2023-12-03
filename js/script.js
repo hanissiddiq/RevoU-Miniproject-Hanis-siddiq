eeeb5bc5d7f29466f8d8c6dbd3d0698e3e3ec480
hanis siddiq <hanissiddiq10@gmail.com>
2023-12-04 00:21:54 +0700

    add reset button & repair value from DOM

--- js/script.js
+++ js/script.js
@@ -14,11 +14,26 @@ function keliling() {
   var s1 = document.getElementById("input-sisi1").value;
   var s2 = document.getElementById("input-sisi2").value;
   var s3 = document.getElementById("input-sisi3").value;
-  var rumuskeliling = s1 + s2 + s3;
+  var rumuskeliling = parseInt(s1) + parseInt(s2) + parseInt(s3);
   console.log(rumuskeliling);
 
   document.getElementById("hasilKeliling").innerHTML = `<ul>
   <li> Keliling = S + S + S</li>
   <li> Hasil = ${rumuskeliling} </li>
 </ul>`;
+}
+
+function fun() {
+  document.getElementById("myForm").reset();
+}
+
+function resetLuas() {
+  document.getElementById("input-tinggi").value = "";
+  document.getElementById("input-alas").value = "";
+}
+
+function resetKeliling() {
+  document.getElementById("input-sisi1").value = "";
+  document.getElementById("input-sisi2").value = "";
+  document.getElementById("input-sisi3").value = "";
 }
